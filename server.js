547b394f62343d02cbc33ce5b22cbe03c6ca21eb
fael-Bruno <148697016+fael-Bruno@users.noreply.github.com>
2025-08-20 20:05:36 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const WebSocket = require("ws");
+
+const PORT = process.env.PORT || 8080;
+
+// cria servidor HTTP (Render precisa disso)
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Servidor WebSocket rodando");
+});
+
+const wss = new WebSocket.Server({ server });
+
+let clients = new Set();
+
+wss.on("connection", (ws) => {
+  clients.add(ws);
+  broadcastOnline();
+
+  ws.on("message", (msg) => {
+    // repassa mensagem para todos
+    for (let client of clients) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(msg.toString());
+      }
+    }
+  });
+
+  ws.on("close", () => {
+    clients.delete(ws);
+    broadcastOnline();
+  });
+});
+
+function broadcastOnline() {
+  const online = clients.size;
+  for (let client of clients) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify({ online }));
+    }
+  }
+}
+
+server.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
